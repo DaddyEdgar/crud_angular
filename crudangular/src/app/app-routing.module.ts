0d0420cce32da96c68cd57d3c21e330c6bf973b2
DaddyEdgar <utp0137638@alumno.utpuebla.edu.mx>
2021-08-29 18:46:17 -0500

    Diseño del sistema

--- crudangular/src/app/app-routing.module.ts
+++ crudangular/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Se importan las rutas de los empleados
+import { AgregarEmpleadoComponent } from './componentes/agregar-empleado/agregar-empleado.component';
+import { EditarEmpleadoComponent } from './componentes/editar-empleado/editar-empleado.component';
+import { ListarEmpleadoComponent } from './componentes/listar-empleado/listar-empleado.component';
+
+//Se agregan las rutas de  los empleados
+const routes: Routes = [
+  //Redirecciona en la raiz si no encuentrá el URL 
+  { path: '', pathMatch: 'full', redirectTo: 'agregar-empleado' },
+  //URL de la página
+  { path: 'agregar-empleado', component: AgregarEmpleadoComponent },
+  { path: 'listar-empleado', component: ListarEmpleadoComponent },
+  { path: 'editar-empleado/:id', component: EditarEmpleadoComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
